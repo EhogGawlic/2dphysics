c49431f436358053919413653e43bc8f056ab198
EhogGawlic <ethang0227@gmail.com>
2025-02-28 20:09:50 -0800

    Update elements.js

--- src/elements.js
+++ src/elements.js
@@ -52,6 +52,7 @@ const ctx = canvas.getContext("2d"),
       clearufbtn= getEl("clearufbtn"),
       savelnk = getEl("savelnk"),
       savebtn = getEl("savebtn"),
+      savebtnc = getEl("savebntc"),
       saveinp = getEl("saveinp"),
       ppbtn = getEl("ppbtn"),
       rstbtn = getEl("rstbtn"),
@@ -61,7 +62,89 @@ const ctx = canvas.getContext("2d"),
       vninp = getEl("vninp"),
       ocvbtn = getEl("ocvbtn"),
       afbtn = getEl("afbtn"),
-      fsinp = getEl("fsinp")
+      fsinp = getEl("fsinp"),
+      stinp = getEl("stinp"),
+      scinp = getEl("scinp"),
+      asinp = getEl("addselect"),
+      dbtn = getEl("dbtn"),
+      stbtn = getEl("settingsbtn"),
+      grinp = getEl("gravinp"),
+      settings = getEl("settings"),
+      tcansrc = getEl("tcansrc"),
+      ceinp = getEl("ceinp"),
+      lninp = getEl("lni"),
+      okbtn = getEl("okbtn1"),
+      msinp = getEl("msinp"),
+      sps = getEl("sps")
 lmenu.style.width = offX+"px"
 rmenu.style.width = offX+"px"
 ctx.lineCap = "round"
+switch(getCookie("btype")){
+    case "\"sm\"":
+        dinp.value=78.3
+        cinp.value="#808080"
+        binp.value=0.2
+        liq = false
+        stinp.value=0
+        rinp.value=5
+        break
+    case "\"pla\"":
+        dinp.value=12.5
+        cinp.value="#F0F0F0"
+        binp.value=0.8
+        liq = false
+        stinp.value=0
+        rinp.value=5
+        break
+    case "\"fb\"":
+        dinp.value=12.5
+        cinp.value="#0080FF"
+        binp.value=1
+        liq = false
+        stinp.value=0
+        rinp.value=5
+        break
+    case "\"nb\"":
+        dinp.value=12.5
+        cinp.value="#FF6961"
+        binp.value=0
+        liq = false
+        stinp.value=0
+        rinp.value=5
+        break
+    case "\"w\"":
+        dinp.value=9.98
+        cinp.value="#004CFF"
+        binp.value=0.05
+        rinp.value=1
+        vxinp.value=300
+        vyinp.value=150
+        acs.value=1
+        liq = true
+        stinp.value=72
+        break
+    case "\"m\"":
+        dinp.value=135.46
+        cinp.value="#B7B8B9"
+        binp.value=0.05
+        rinp.value=1
+        vxinp.value=300
+        vyinp.value=150
+        acs.value=1
+        liq = true
+        stinp.value=72
+        break
+    case "\"sf\"":
+        dinp.value=0.35
+        cinp.value="#F9F9F9"
+        binp.value=0.1
+        liq = false
+        rinp.value=5
+        stinp.value=0
+}
+const fan1 = imgSrc("fan1.png")
+const fan2 = imgSrc("fan2.png")
+const savestr = getStorage("save")
+if (savestr){
+    decode(savestr)
+}
